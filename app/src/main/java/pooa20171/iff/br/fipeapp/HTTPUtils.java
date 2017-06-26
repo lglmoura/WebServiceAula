3248a329a072083436446d702c197eb645eadd8a
Luiz Gustavo <lglmoura@gmail.com>
2017-06-26 20:20:17 -0300

    Projeto todo

---
+++ app/src/main/java/pooa20171/iff/br/fipeapp/HTTPUtils.java
@@ -0,0 +1,35 @@
+package pooa20171.iff.br.fipeapp;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
+/**
+ * Created by lglmo on 26/06/2017.
+ */
+
+public class HTTPUtils {
+
+    public static String acessar(String endereco){
+
+        try{
+            URL url = new URL(endereco);
+
+            URLConnection conn = url.openConnection();
+            InputStream is = conn.getInputStream();
+
+            Scanner scanner = new Scanner(is);
+
+            String conteudo = scanner.useDelimiter("\\A").next();
+
+            scanner.close();
+
+            return conteudo;
+
+        }catch (Exception e){
+            return null;
+        }
+
+    }
+}
